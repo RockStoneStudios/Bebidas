bf66b1e0a1eb3c74bb1ff3c4526179c47fbce1f8
RockStone <RockStoneStudios666@gmail.com>
2022-03-12 20:36:00 -0500

    Fin.

---
+++ src/components/Bebida.jsx
@@ -0,0 +1,34 @@
+import React, { useState } from 'react'
+import { Col, Card, Button } from 'react-bootstrap';
+import useBebidas from '../hooks/useBebidas';
+
+const Bebida = ({bebida}) => {
+ const {handleModalClick,handleBebidaIdClick} = useBebidas();
+
+  return (
+   <Col md={2} lg={3}>
+    <Card className='mb-4'>
+        <Card.Img 
+        variant='top'
+        src={bebida.strDrinkThumb}  
+        alt = {`Imagen de ${bebida.strDrink}`}
+     />
+
+     <Card.Body>
+         <Card.Title>{bebida.strDrink}</Card.Title>
+         <Button variant='warning'
+          className='w-100 text-uppercase'
+          onClick={()=>{
+            handleModalClick()
+            handleBebidaIdClick(bebida.idDrink);
+          }}
+         >ver Receta</Button>
+     </Card.Body>
+          
+ 
+    </Card>
+   </Col>
+  )
+}
+
+export default Bebida;
